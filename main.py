4e79dfa9f18d5eed9d911afa36a6caed3bde9882
bceverly <bryan@theeverlys.com>
2024-07-05 10:06:06 -0400

    Added loop

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ This module is the main entry point for the SysManage agent that will run
 on all clients.  It connects back to the server and communicates
 bidirectionally over WebSockets.
 """
+from time import sleep
 from websockets.sync.client import connect
 
 if __name__ == "__main__":
     with connect("wss://api.sysmanage.org:6443/agent/connect") as websocket:
-        websocket.send("Hello world!")
-        message = websocket.recv()
-        print(f"Received: {message}")
+        while True:
+            sleep(5)
+            websocket.send("Hello world!")
+            message = websocket.recv()
+            print(f"Received: {message}")
